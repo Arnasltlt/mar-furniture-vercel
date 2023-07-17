aa6e7ac804d77fff7bcffa8f8d6ef12ffce47082
Arnasltlt <arnoldas.kemeklis@gmail.com>
2023-07-17 17:01:39 -0500

    Update airtableForm.ts

--- lib/airtableForm.ts
+++ lib/airtableForm.ts
@@ -3,7 +3,7 @@ import Airtable from 'airtable';
 const api_secret = process.env.AIRTABLE_API_KEY
 const base_secret = process.env.AIRTABLE_BASE
 
-const base = new Airtable({ apiKey: api_secret }).base(base_secret);
+const base = new Airtable({ apiKey: api_secret }).base('appcYcJWTWr6eedym');
 
 export const createUzklausimai = async (vardas: string, uzklausa: string, email: string) => {
   return new Promise((resolve, reject) => {
