af7e044a019ad2d52289d2661919c8bc3684f92a
Arnasltlt <arnoldas.kemeklis@gmail.com>
2023-07-18 19:37:13 -0500

    Update CookieConsentBanner.tsx

--- components/CookieConsentBanner.tsx
+++ components/CookieConsentBanner.tsx
@@ -1,34 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/CookieConsentBanner.module.css';
 
 const CookieConsentBanner: React.FC = () => {
-    const [consentGiven, setConsentGiven] = useState(null);
-    const [bannerStatus, setBannerStatus] = useState('entering');
+  const [consentGiven, setConsentGiven] = useState(false);
+  
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const consent = !!localStorage.getItem('cookieConsent');
+      setConsentGiven(consent);
+    }
+  }, []);
 
-    useEffect(() => {
-      setConsentGiven(!!localStorage.getItem('cookieConsent'));
-    }, []);
+  useEffect(() => {
+    if (consentGiven) {
+      setTimeout(() => {
+        localStorage.setItem('cookieConsent', 'true');
+      }, 300);
+    }
+  }, [consentGiven]);
 
-    useEffect(() => {
-      if (consentGiven) {
-        setBannerStatus('exiting');
-        setTimeout(() => setBannerStatus('exited'), 300);
-      }
-    }, [consentGiven]);
+  if (consentGiven) return null;
 
-    if (consentGiven === null || bannerStatus === 'exited') return null;
-
-    const giveConsent = () => {
-      localStorage.setItem('cookieConsent', 'true');
-      setConsentGiven(true);
-    };
-
-    return (
-      <div className={`${styles['cookie-banner']} ${styles[bannerStatus]}`}>
-        <p>Mes naudojame slapukus, kad pagerintume jūsų patirtį. Tęsdami naršymą šioje svetainėje, jūs sutinkate su mūsų slapukų politika.</p>
-        <button onClick={giveConsent}>Sutinku ir uždaryti</button>
-      </div>
-    );
+  return (
+    <div className={`${styles['cookie-banner']} ${consentGiven ? styles.exiting : ''}`}>
+      <p>Mes naudojame slapukus, kad pagerintume jūsų patirtį. Tęsdami naršymą šioje svetainėje, jūs sutinkate su mūsų slapukų politika.</p>
+      <button onClick={() => setConsentGiven(true)}>Sutinku ir uždaryti</button>
+    </div>
+  );
 }
 
 export default CookieConsentBanner;
